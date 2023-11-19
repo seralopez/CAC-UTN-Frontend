4a8856e32b32652aea000dbd4c139207979a5fed
seralopez <sergio.alb.lopez@gmail.com>
2023-11-18 23:43:23 -0300

    Iniciando Frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { ResultadosComponent } from './pages/resultados/resultados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'resultados', component: ResultadosComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResultadosComponent } from './pages/resultados/resultados.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultadosComponent,
+    PerfilComponent,
+    ContactoComponent,
+    InicioComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
