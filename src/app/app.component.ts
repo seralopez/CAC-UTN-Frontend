76abad6a6535c312d9b74bfc427073eaf24c8618
seralopez <sergio.alb.lopez@gmail.com>
2023-11-30 00:36:36 -0300

    Buscador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { IServices } from './models/servicio.model';
+import { ActivatedRoute, Params } from '@angular/router';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +16,41 @@ export class AppComponent {
     { buscar: new FormControl('') }
   )
 
+  public listaProvedores: IServices[] = []
+  loading: boolean = true
+  error: string = ''
+
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _apiService: ApiService
+
+  ) { }
+
+
+
+
   enviarInput(event: Event) {
     event.preventDefault();
-    console.log(this.formBuscador.value);
+
+    this._activatedRoute.params.subscribe({
+      next: (params: Params) => {
+        this._apiService.getPrestadores('buscador', String(this.formBuscador.value.buscar)).subscribe({
+          next: (data: IServices[]) => {
+            this.listaProvedores = data
+            this.loading = false
+          },
+          error: (error: any) => {
+            this.error = error.error.msg
+            console.log(error.error.msg)
+          }
+        })
+      },
+      error: (error: any) => {
+        console.log(error.error.msg)
+      }
+    })
+
+
+
   }
 }
